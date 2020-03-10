d972c9baa05eed3aa91cdb984b3660ea7c4a6d30
king2509 <624803339@qq.com>
2020-03-10 14:51:23 +0800

    Add list, nlist functions

--- MyFTPClient.java
+++ MyFTPClient.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.StringTokenizer;
 
 public class MyFTPClient {
     private boolean DEBUG = true;
     private Socket socket = null;
+    private Socket dataSocket = null;
     private BufferedReader reader = null;
     private BufferedWriter writer = null;
+    private String response = null;
 
-    public MyFTPClient(){
+    private MyFTPClient(){
 
     }
 
@@ -24,7 +27,7 @@ public class MyFTPClient {
         socket = new Socket(ip, port);
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        String response = readLine();
+        response = readLine();
         if(!response.startsWith("220")) {
             throw new IOException(
                     "MyFTPClient received a unknown response: "
@@ -37,7 +40,7 @@ public class MyFTPClient {
         if(socket == null)
             throw new IOException("Connect First");
         sendLine("USER "+username);
-        String response = readLine();
+        response = readLine();
         if(!response.startsWith("331 ")) {
             throw new IOException(
                     "MyFTPClient received a unknown response after sending USERNAME "
@@ -51,12 +54,46 @@ public class MyFTPClient {
                             + response);
         }
         System.out.println("Logged successfully");
+        try{
+            establishDataSocket();
+            System.out.println("Establish dataSocket successfully");
+        } catch (IOException e) {
+            throw new IOException("Failed to establish dataSocket");
+        }
     }
+
+    // get dataSocket to transfer data
+    private synchronized void establishDataSocket() throws IOException{
+        String remoteIp = null;
+        int remotePort = -1;
+        if(dataSocket != null)
+            return;
+        sendLine("PASV");
+        response = readLine();
+        if(!response.startsWith("227")) {
+            throw new IOException("Server could not get into PASSIVE MODE");
+        }
+        int opening = response.indexOf('(');
+        int closing = response.indexOf(')');
+        if(closing > 0) {
+            String ipAndPort = response.substring(opening + 1, closing);
+            StringTokenizer dataLink = new StringTokenizer(ipAndPort, ",");
+            try{
+                remoteIp = dataLink.nextToken() + "." + dataLink.nextToken()
+                        + "." + dataLink.nextToken() + "." + dataLink.nextToken();
+                remotePort = Integer.parseInt(dataLink.nextToken()) * 256 + Integer.parseInt(dataLink.nextToken());
+            } catch (Exception e) {
+                throw new IOException("DataLink broken");
+            }
+        }
+        dataSocket = new Socket(remoteIp, remotePort);
+    }
+
 
     // change working directory
     private synchronized boolean cwd(String dir) throws IOException{
         sendLine("CWD "+ dir);
-        String response = readLine();
+        response = readLine();
         System.out.println(response);
         if(response.startsWith("550"))
             throw new IOException("The system cannot find the file specified");
@@ -94,34 +131,12 @@ public class MyFTPClient {
         String filename = file.getName();
         FileInputStream inputStream = new FileInputStream(file);
         BufferedInputStream input = new BufferedInputStream(inputStream);
-        sendLine("PASV");
-        String response = readLine();
-        if(!response.startsWith("227")) {
-            throw new IOException("Server could not get into PASSIVE MODE");
-        }
-        String ip = null;
-        int port = -1;
-        int opening = response.indexOf('(');
-        int closing = response.indexOf(')');
-        if(closing > 0) {
-            String ipAndPort = response.substring(opening + 1, closing);
-            StringTokenizer dataLink = new StringTokenizer(ipAndPort, ",");
-            try{
-                ip = dataLink.nextToken() + "." + dataLink.nextToken()
-                        + "." + dataLink.nextToken() + "." + dataLink.nextToken();
-                port = Integer.parseInt(dataLink.nextToken()) * 256 + Integer.parseInt(dataLink.nextToken());
-            } catch (Exception e) {
-                throw new IOException("DataLink broken");
-            }
-        }
-
         sendLine("STOR " + filename);
-        Socket dataSocket = new Socket(ip, port);
         response = readLine();
-        if(!response.startsWith("150"))
-            throw new IOException("Failed to store file");
+        //if(!response.startsWith("150"))
+        //    throw new IOException("Failed to store file");
         BufferedOutputStream output = new BufferedOutputStream(dataSocket.getOutputStream());
-        long fileSize = file.length() / 4096;
+        long fileSize = file.length();
         System.out.println("fileSize: " + fileSize);
         long cnt = 0;
         byte[] buffer = new byte[4096];
@@ -137,18 +152,74 @@ public class MyFTPClient {
         return response.startsWith("226 ");
     }
 
+    // cd upper directory
+    // if pwd==root, cdup success as well.
+    private synchronized boolean cdup() throws IOException{
+        sendLine("CDUP");
+        response = readLine();
+        return response.startsWith("250");
+    }
+
+    private synchronized boolean renameFile(String oldFilename, String newFilename) throws IOException{
+        sendLine("RNFR " + oldFilename);
+        response = readLine();
+        if(!response.startsWith("350"))
+            throw new IOException("No target file");
+        sendLine("RNTO " + newFilename);
+        response = readLine();
+        return response.startsWith("250");
+    }
+
+    // mkdir  needs to check if directory exists
+    private synchronized boolean mkdir(String directoryName) throws IOException{
+        sendLine("MKD "+ directoryName);
+        response = readLine();
+        return response.startsWith("257");
+    }
+
+    // list all names of files & directories under directory specified
+    private synchronized boolean getNLST(String dir) throws IOException{
+        sendLine("NLST " + dir);
+        InputStreamReader inputStreamReader = new InputStreamReader(dataSocket.getInputStream(), StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        dataSocket.getInputStream().close();
+        inputStreamReader.close();
+        return true;
+    }
+
+    // list details of all files and directories under directory specified
+    private synchronized boolean getList(String dir) throws IOException{
+        sendLine("LIST " + dir);
+        InputStreamReader inputStreamReader = new InputStreamReader(dataSocket.getInputStream(), StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        dataSocket.getInputStream().close();
+        inputStreamReader.close();
+        return true;
+    }
+
+    // Mode binary: mp4, jpg, ...
     private synchronized boolean bin() throws IOException{
         sendLine("TYPE A");
         String response = readLine();
         return response.startsWith("200");
     }
 
+    // Mode ascii: txt
     private synchronized boolean ascii() throws IOException{
         sendLine("TYPE I");
         String response = readLine();
         return response.startsWith("200");
     }
 
+    // read response from server
     private String readLine() throws IOException{
         if(socket == null)
             throw new IOException("socket needs to be connected");
@@ -158,6 +229,7 @@ public class MyFTPClient {
         return line;
     }
 
+    // send request to server
     private void sendLine(String cmd) throws IOException{
         if(socket == null)
             throw new IOException("socket needs to be connected");
@@ -178,8 +250,13 @@ public class MyFTPClient {
         myFTPClient.connect();
         myFTPClient.login("king","0925");
         //myFTPClient.bin();
-        myFTPClient.ascii();
-        myFTPClient.stor(new File("D:/king.mp4"));
+        //myFTPClient.ascii();
+        //myFTPClient.stor(new File("D:/king.txt"));
+        //myFTPClient.renameFile("kong.txt", "king.txt");
+        //System.out.println(myFTPClient.pwd());
+        //myFTPClient.mkdir("NewFolder");
+        //myFTPClient.getNLST("/");
+        myFTPClient.getList("/");
         myFTPClient.close();
     }
 }
